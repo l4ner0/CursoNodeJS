e520f29b1f01ee8f5f376f5cc58dea9f16ef4b54
Diego Urbina <diego.urbina@acceso.com.pe>
2022-01-02 22:31:11 -0500

    Clase 33 y 34

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body>');
         res.write('</html>');
+        return res.end();
+    }
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', chunk => {
+            console.log('chunk: ', chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parseBody = Buffer.concat(body).toString();
+            console.log('parseBody: ', parseBody);
+            const message = parseBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });        
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
         return res.end();
     }
 
