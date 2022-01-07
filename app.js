6bec4144652e286c77e109502923130cf901cbc3
Diego Urbina <diego.urbina@acceso.com.pe>
2022-01-06 22:43:22 -0500

    Clase 36

--- app.js
+++ app.js
@@ -17,15 +17,17 @@ const server = http.createServer((req, res) => {
             console.log('chunk: ', chunk);
             body.push(chunk);
         });
-        req.on('end', () => {
+        return req.on('end', () => {
             const parseBody = Buffer.concat(body).toString();
             console.log('parseBody: ', parseBody);
             const message = parseBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-        });        
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
+            /* fs.writeFileSync('message.txt', message); */
+            fs.writeFile('message.txt', message, err => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        });
     }
 
     res.setHeader('Content-Type', 'text/html');
